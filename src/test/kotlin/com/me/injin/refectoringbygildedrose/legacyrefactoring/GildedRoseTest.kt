dd2ef6734931e851a74975a8ae700a1a335f74d3
injinjeong <snowwwwww@naver.com>
2022-11-11 22:07:25 +0900

    여러 조합으로 테스트 해야할 시 - 조건은 모두 찾았으나, branch 커버리지 88%

--- src/test/kotlin/com/me/injin/refectoringbygildedrose/legacyrefactoring/GildedRoseTest.kt
+++ src/test/kotlin/com/me/injin/refectoringbygildedrose/legacyrefactoring/GildedRoseTest.kt
@@ -1,7 +1,6 @@
 package com.me.injin.refectoringbygildedrose.legacyrefactoring
 
-import org.approvaltests.Approvals
-import org.assertj.core.api.Assertions.assertThat
+import org.approvaltests.combinations.CombinationApprovals
 import org.junit.jupiter.api.Test
 
 
@@ -9,14 +8,27 @@ class GildedRoseTest {
 
     @Test
     fun updateQuality() {
-        //given
-        val sut = GildedRose(arrayOf(Item("foo", 0, 0)))
-        //when
-        sut.updateQuality()
-        //then
-        assertThat(sut.items[0].toString()).isEqualTo("Item(name=foo, sellIn=-1, quality=0)")
+//        //given
+//        val sut = GildedRose(arrayOf(Item("foo", 0, 0)))
+//        //when
+//        sut.updateQuality()
+//        //then
+//        assertThat(sut.items[0].toString()).isEqualTo("Item(name=foo, sellIn=-1, quality=0)")
+//
+//        //approval test
+//        Approvals.verify(sut.items[0].toString())
 
-        //approval test
-        Approvals.verify(sut.items[0].toString())
+        val name = arrayOf("foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros")
+        val sellIn = arrayOf(-1, 0, 11)
+        val quality = arrayOf(0, 1, 50)
+
+        //여러 조합으로 테스트 해야할 시
+        CombinationApprovals.verifyAllCombinations(this::doUpdateQuality, name, sellIn, quality)
+    }
+
+    fun doUpdateQuality(name: String, sellIn: Int, quality: Int): String {
+        val sut = GildedRose(arrayOf(Item(name, sellIn, quality)))
+        sut.updateQuality()
+        return sut.items[0].toString()
     }
 }
