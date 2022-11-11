53483743fda3e97003182ce03659d69c4bca8821
injinjeong <snowwwwww@naver.com>
2022-11-11 23:53:43 +0900

    test coverage 100% 맞췄지만 버그가 존재하고, sellIn 값에 1, 6 을 추가해야함. pitest 는 도저히 안되서 넘어감..

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,17 @@ plugins {
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+
+    //pitest
+    id("info.solidsoft.pitest") version "1.5.1"
 }
 
 group = "com.me.injin"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+
+//pitest
+apply(plugin = "info.solidsoft.pitest")
 
 repositories {
     mavenCentral()
@@ -23,6 +29,10 @@ dependencies {
 
     //approval test
     testImplementation("com.approvaltests:approvaltests:18.5.0")
+
+    testImplementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.5.1")
+    pitest ("com.groupcdg.pitest:pitest-kotlin-plugin:0.2.2")
+
 }
 
 tasks.withType<KotlinCompile> {
@@ -34,4 +44,22 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+pitest {
+//    setProperty("junit5PluginVersion", "0.12")
+//    setProperty("testPlugin", "junit5")
+//    targetClasses.set(setOf("${project.group}.*"))  //by default "${project.group}.*"
+//
+//    setProperty("targetClasses", listOf("target package"))
+//    setProperty("outputFormats", listOf("XML", "HTML"))
+
+    junit5PluginVersion.set("0.12")
+    avoidCallsTo.set(setOf("kotlin.jvm.internal"))
+//    mutators.set(setOf("STRONGER"))
+    targetClasses.set(setOf("com.me.injin.refectoringbygildedrose.legacyrefactoring.*"))  //by default "${project.group}.*"
+    targetTests.set(setOf("com.me.injin.refectoringbygildedrose.legacyrefactoring.*Test"))
+//    pitestVersion.set("1.7.4")   //current defined for Gradle plugin PIT version should be used
+    threads.set(Runtime.getRuntime().availableProcessors())
+    outputFormats.set(setOf("XML", "HTML"))
 }
--- src/test/kotlin/com/me/injin/refectoringbygildedrose/legacyrefactoring/GildedRoseTest.kt
+++ src/test/kotlin/com/me/injin/refectoringbygildedrose/legacyrefactoring/GildedRoseTest.kt
@@ -8,18 +8,8 @@ class GildedRoseTest {
 
     @Test
     fun updateQuality() {
-//        //given
-//        val sut = GildedRose(arrayOf(Item("foo", 0, 0)))
-//        //when
-//        sut.updateQuality()
-//        //then
-//        assertThat(sut.items[0].toString()).isEqualTo("Item(name=foo, sellIn=-1, quality=0)")
-//
-//        //approval test
-//        Approvals.verify(sut.items[0].toString())
-
         val name = arrayOf("foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros")
-        val sellIn = arrayOf(-1, 0, 11)
+        val sellIn = arrayOf(-1, 0, 1, 6, 11)
         val quality = arrayOf(0, 1, 49, 50)
 
         //여러 조합으로 테스트 해야할 시
