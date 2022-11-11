9a08b71b3669c783a922d05cb970347f9e8434f4
injinjeong <snowwwwww@naver.com>
2022-11-11 20:21:57 +0900

    first commit

---
+++ src/main/kotlin/com/me/injin/refectoringbygildedrose/RefectoringByGildedRoseApplication.kt
@@ -0,0 +1,11 @@
+package com.me.injin.refectoringbygildedrose
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RefectoringByGildedRoseApplication
+
+fun main(args: Array<String>) {
+    runApplication<RefectoringByGildedRoseApplication>(*args)
+}
---
+++ src/test/kotlin/com/me/injin/refectoringbygildedrose/RefectoringByGildedRoseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.me.injin.refectoringbygildedrose
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RefectoringByGildedRoseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
