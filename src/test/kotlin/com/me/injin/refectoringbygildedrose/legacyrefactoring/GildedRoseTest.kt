04307159578cbbd1b13481898a2d0dcf087c3eda
injinjeong <snowwwwww@naver.com>
2022-11-11 22:10:20 +0900

    커버리지100% 맞춘..

--- src/test/kotlin/com/me/injin/refectoringbygildedrose/legacyrefactoring/GildedRoseTest.kt
+++ src/test/kotlin/com/me/injin/refectoringbygildedrose/legacyrefactoring/GildedRoseTest.kt
@@ -20,7 +20,7 @@ class GildedRoseTest {
 
         val name = arrayOf("foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros")
         val sellIn = arrayOf(-1, 0, 11)
-        val quality = arrayOf(0, 1, 50)
+        val quality = arrayOf(0, 1, 49, 50)
 
         //여러 조합으로 테스트 해야할 시
         CombinationApprovals.verifyAllCombinations(this::doUpdateQuality, name, sellIn, quality)
